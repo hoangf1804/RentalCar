a14bfd64f500d93be4d1e91712792bc70b00a33f
leexuaanhoangf <akbachkim@gmail.com>
2025-10-09 12:09:45 +0700

    Revert "kết nối cơ sở dữ liệu"
    This reverts commit 696dd1b5ae819fcb81b206e5c81cc8b7c83f2d54.

--- rentalcar/src/main/java/com/rentalcar/rentalcar/RentalcarApplication.java
+++ rentalcar/src/main/java/com/rentalcar/rentalcar/RentalcarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RentalCarApplication {
+public class RentalcarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RentalCarApplication.class, args);
+        SpringApplication.run(RentalcarApplication.class, args);
     }
 
 }
