850780f455e5c07fe7bbfeea694b90aed0ef5e0c
leexuaanhoangf <akbachkim@gmail.com>
2025-10-09 11:30:31 +0700

    Tạo dự án

--- rentalcar/src/main/java/com/rentalcar/rentalcar/RentalCarApplication.java
+++ rentalcar/src/main/java/com/rentalcar/rentalcar/RentalCarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RentalcarApplication {
+public class RentalCarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RentalcarApplication.class, args);
+        SpringApplication.run(RentalCarApplication.class, args);
     }
 
 }
